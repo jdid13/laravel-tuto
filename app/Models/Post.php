f48a31044f8a770e627175dfc151b1a65b56476d
jdid13 <dwwm-nadir-benadda@mode83.onmicrosoft.com>
2023-09-11 15:07:07 +0200

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+
+class Post {
+    public static function find($slug) {
+        $path = resource_path("posts/{$slug}.html");
+
+        if (!file_exists($path)) {
+            throw new ModelNotFoundException;
+        }
+
+        $post = cache() -> remember("posts/{$slug}", 1200, fn() => file_get_contents($path));
+        
+
+        return $post;
+    }   
+
+    public static function all() {
+        $files = File::files(resource_path("posts/"));
+
+        return array_map(fn($file) => $file->getContents(), $files);
+    }   
+}
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    $document = YamlFrontMatter::parseFile(
+        resource_path('posts/my-fourth-post.html')
+    );
+
+    // return view('posts', [
+    //     'posts' => Post::all()
+    // ]);
+    ddd($document->matter('title'));
+});
+
+Route::get('posts/{post}', function ($slug) {
+    // Find a post by its slug and pass it to a view called "post"
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
+}) -> where('post', '[A-z_\-]+');
